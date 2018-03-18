671caa2cd59855637b89ea1519b2147730e9f95d
William Cory <willcory10@gmail.com>
2018-03-17 22:52:36 -0700

    complete helper functions for lineup class

--- optimizer.js
+++ optimizer.js
@@ -16,13 +16,40 @@ const Lineup = function() {
   this.failed = false;
 }
 
-Lineup.prototype.createKey = function(currentPlayer) {
+const fdRoster = ['pg1', 'pg2', 'sg1', 'sg2', 'sf1', 'sf2', 'pf1', 'pf2', 'c'];
 
+Lineup.prototype.createKey = function(currentPlayer) {
+  return JSON.stringify(fdRoster.map(pos => this.roster[pos].length)) + currentPlayer;
 }
 
 Lineup.prototype.mergeLineups = function(lineup) {
+  Object.keys(lineup).forEach(pos => {
+    if (!this.roster[pos]) {
+      this.roster[pos] = lineup.roster[pos];
+    }
+  })
+  return this;
+};
 
-}
+Lineup.prototype.addPlayer = function(player) {
+  const addPosition = {
+    'pg': ['pg1', 'pg2'],
+    'sg': ['sg1', 'sg2'],
+    'sf': ['sf1', 'sf2'],
+    'pf': ['pf1', 'pf2'],
+    'c': ['c']
+  }
+
+  addPosition[player.position].forEach(pos => {
+    if (!this.roster[pos]) {
+      this.roster[pos] = player;
+    }
+  });
+  this.salary += player.salary;
+  this.projection += player.projection;
+
+  return this;
+};
 
 let memoize = {};
 
@@ -33,11 +60,10 @@ const optimize = function(playerPool, currentLineup, currentPlayer) {
     return currentLineup.mergeLineups(memoize[dicKey]);
   }
 
-  lineupIfPass = optimize(playerPool, currentLineup, currentPlayer + 1);
+  const lineupIfPass = optimize(playerPool, currentLineup, currentPlayer + 1);
+  const lineupIfTake = optimize(playerPool, currentLineup.addPlayer(playerPool, currentPlayer), currentPlayer + 1);
 
-  lineupIfTake = optimize(playerPool, currentLineup.add(playerPool, currentPlayer), currentPlayer + 1);
-
-  let out;
+  const out;
   if (lineupIfPass.failed || lineupIfTake.failed) {
     out = lineupIfPass.failed ? lineupIfTake : lineupIfPass;
   } else {
@@ -46,6 +72,7 @@ const optimize = function(playerPool, currentLineup, currentPlayer) {
 
   memoize[dicKey] = out;
   return out;
+
 };
 
 
